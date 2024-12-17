b150610183d91e024342b3d32f333eaf7a9e5c1b
DESKTOP-HME3UEB\403megait <test@test.com>
2024-12-17 18:23:21 +0900

    MVC 흐름 예제1

---
+++ src/main/java/com/example/lesson04/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package com.example.lesson04.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.example.lesson04.domain.User;
+
+@Mapper
+public interface UserMapper {
+
+	// i:4개 파라미터
+	// o:int or void
+	public void insertUser(
+			@Param("name") String name, 
+			@Param("yyyymmdd") String yyyymmdd,
+			@Param("email") String email,
+			@Param("introduce") String introduce);
+	
+	// i:X
+	// o:User or null
+	public User selectLatestUser();
+}
+
+
+
+
