a6bf79c23f4bab9d499edd6beef4ff10b939836a
DESKTOP-HME3UEB\403megait <test@test.com>
2025-01-02 17:53:00 +0900

    select 쿼리 메소드 규칙

--- src/main/java/com/example/lesson07/repository/StudentRepository.java
+++ src/main/java/com/example/lesson07/repository/StudentRepository.java
@@ -1,5 +1,7 @@
 package com.example.lesson07.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.lesson07.entity.StudentEntity;
@@ -10,4 +12,16 @@ public interface StudentRepository extends JpaRepository<StudentEntity, Integer>
 	//public StudentEntity save(StudentEntity se); // insert(id가 0) or update(id 0보다 크면)
 	//public Optional<StudentEntity> findById(int id);
 	//public void delete(StudentEntity se);
+	//public List<StudentEntity> findAll();
+	
+	// ex02 select  - JPQL을 만들어내는 메소드명 규칙
+	public List<StudentEntity> findAllByOrderByIdDesc();
+	public List<StudentEntity> findTop3ByOrderByIdDesc();
+	public List<StudentEntity> findByName(String name111343);
+	public List<StudentEntity> findByNameIn(List<String> names);
+	public List<StudentEntity> findByNameAndDreamJob(String name22, String dreamJob1);
+	public List<StudentEntity> findByEmailContains(String keyword);
+	public List<StudentEntity> findByNameStartingWith(String keyword);
+	public List<StudentEntity> findByIdBetween(int startId, int endId);
+	
 }
